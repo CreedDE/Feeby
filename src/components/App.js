8725ce68cfe473bc227eac8e0ee97e9e8eb2d898
Tim <41332362+CreedDE@users.noreply.github.com>
2018-09-17 11:00:12 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+/* import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
+import { applyMiddleware, createStore } from 'redux' 
+import reducer from '../reducer'
+import thunk from 'redux-thunk' */
+
+class App extends Component {
+  render() {
+    return <div>Hello World Wupp</div>
+  }
+}
+
+export default App
